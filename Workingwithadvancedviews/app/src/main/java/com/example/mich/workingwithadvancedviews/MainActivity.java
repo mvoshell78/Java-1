5fe96c5fc736282ffd8e3bc1825c29740e63e813
mvoshell78 <michael.voshell@yahoo.com>
2015-12-07 23:01:04 -0500

    clean up

--- Workingwithadvancedviews/app/src/main/java/com/example/mich/workingwithadvancedviews/MainActivity.java
+++ Workingwithadvancedviews/app/src/main/java/com/example/mich/workingwithadvancedviews/MainActivity.java
@@ -1,3 +1,8 @@
+// Michael Voshell
+//week 3 project
+
+
+
 package com.example.mich.workingwithadvancedviews;
 
 import android.os.Bundle;
@@ -8,6 +13,7 @@ import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
+    // sets up an arrayList of the workers class
     private ArrayList<Workers> mWorkers;
 
 
@@ -18,15 +24,20 @@ public class MainActivity extends AppCompatActivity {
 
 
         TextView testView = (TextView) findViewById(R.id.textView);
-
+        // creates a new arrayList called mWorkers
         mWorkers = new ArrayList<>();
+
+        // adds the items to the list
         mWorkers.add(new Workers("Bev Macdonald", "Loading"));
         mWorkers.add(new Workers("Dana Steele", "Order Picking"));
         mWorkers.add(new Workers("Eric Malascalza", "Fueling"));
 
-
+        // checks is the arraylist has contents
         if (!(mWorkers.isEmpty())){
             testView.setText("Array List is loaded");
+
+        } else {
+            testView.setText("Array List is empty");
         }
     }
 }
--- Workingwithadvancedviews/app/src/main/java/com/example/mich/workingwithadvancedviews/Workers.java
+++ Workingwithadvancedviews/app/src/main/java/com/example/mich/workingwithadvancedviews/Workers.java
@@ -1,35 +1,26 @@
 package com.example.mich.workingwithadvancedviews;
 
-/**
- * Created by Mich on 12/7/15.
- */
-public class Workers {
-    // Member variables
 
+public class Workers {
+
+    // Member variables
     private String mName;
     private String mDepartment;
 
-    // Class constructor
-       /* public Workers() {
-            mYearsOfService = 0;
-            mName = mDepartment = "";
-        }*/
 
+    //creates the object
     public Workers(String _name, String _department ) {
 
         mName = _name;
         mDepartment = _department;
 
     }
+
     // Getter methods.
-
-
     public String getName() { return mName;}
     public String getDepartment() { return mDepartment;}
 
     // Setter Methods
-
-
     public void setName(String _name) { mName = _name; }
     public void setDepartment(String _department) { mDepartment = _department; }
 
