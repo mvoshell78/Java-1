d8e3f07bc09d6042c34e3ec68e7df8fe3069491e
mvoshell78 <michael.voshell@yahoo.com>
2015-12-10 16:10:50 -0500

    final
    added on click to spinner
    added UI population method

--- Workingwithadvancedviews/app/src/main/java/com/example/mich/workingwithadvancedviews/MainActivity.java
+++ Workingwithadvancedviews/app/src/main/java/com/example/mich/workingwithadvancedviews/MainActivity.java
@@ -19,48 +19,71 @@ import java.util.HashMap;
 
 
 public class MainActivity extends AppCompatActivity {
+
     // sets up an arrayList of the workers class
     private ArrayList<Workers> mWorkers;
+
+    //set up var for my list and spinner
     private ListView mListView;
     private Spinner mSpinner;
-    long position = 0;
 
+    //needed in the adapter to make it work
+    long lineNumber = 0;
+
+
+    // setsup the hashmap
     ArrayList<HashMap<String, String>> elements = new ArrayList<HashMap<String,String>>();
+
+
+
+    // simple adapter
     private void setSimpleAdapter(){
 
-        // Field identifiers
+        // var to hold the text in the list/spinner
         final String name = "name";
-        final String department = "department";
-        //final String years = "years";
+        final String jobRes = "department";
+        final String phone = "phone";
+        final String bio = "bio";
 
-        // List of elements for our adapter.
 
 
-        // Goes through each employee and maps the data elements to a String key.
+
+
+        // for loop to fill the array
         for (Workers employee : mWorkers) {
 
             HashMap<String, String> map = new HashMap<String, String>();
+
             map.put(name, employee.getName());
-            map.put(department, employee.getDepartment());
+            map.put(jobRes, employee.getJobRes());
+            map.put(phone, employee.getPhone());
+            map.put(bio, employee.getBio());
 
             elements.add(map);
         }
 
         // Creating an array of our keys
         String[] keys = new String[]{
-                name, department
+                name, jobRes, phone, bio,
         };
 
-        // Creating an array of our list item components.
-        // Indices must match the keys array.
+        // ties the xml file to the spinner/list
         int[] views = new int[]{
                 R.id.employee_name,
                 R.id.employee_department,
 
+                // if you wanted the phone in the list or spinner uncomment the following code
+                // R.id.employee_phone,
+
         };
+
+        // var to figure out the screen rotation
         final int screenOrientation = getResources().getConfiguration().orientation;
 
+        //if to execute pased on orientation
+        //2 equals landscape
         if (screenOrientation == 2){
+
             // Creating a new SimpleAdapter that maps values to views using our keys and views arrays.
             SimpleAdapter adapter = new SimpleAdapter(this, elements, R.layout.listitem, keys, views);
 
@@ -82,18 +105,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        final TextView testView = (TextView) findViewById(R.id.testView);
-        final TextView portTestView = (TextView) findViewById(R.id.portTestView);
+
         // creates a new arrayList called mWorkers
         mWorkers = new ArrayList<>();
 
         // adds the items to the list
-        mWorkers.add(new Workers("Bev Macdonald", "Loading"));
-        mWorkers.add(new Workers("Dana Steele", "Order Picking"));
-        mWorkers.add(new Workers("Eric Malascalza", "Fueling"));
-        mWorkers.add(new Workers("Mich Voshell", "Manager"));
+        mWorkers.add(new Workers("Bev Macdonald", "Loader","(302) 332-5631", "Bev is 34 years old and a mother of 6. She has been with our comaont for 8 years and is a very effective worker with a bright future."));
+        mWorkers.add(new Workers("Dana Steele", "Order Picking","(302) 241-5638", "Dana is 27 and is our part time worker. She currently enjoys knitting and skydiving simultaniouly. she has been with us for 4 years and want to be full time"));
+        mWorkers.add(new Workers("Eric Malascalza", "Fueling","(302) 734-2981", "Eric is 29 and has been with us for over 10 years. he is currently our senior MH and wants to some day be all grown up and run the place."));
+        mWorkers.add(new Workers("Mich Voshell", "Manager","(302) 653-2332", "Mich is the baddest mojang in town he ahs been with us longer than the founders. Some day he may run the joint."));
 
         final int screenOrientation = getResources().getConfiguration().orientation;
+
         if (screenOrientation == 2) {
 
 
@@ -104,11 +127,9 @@ public class MainActivity extends AppCompatActivity {
             mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                 @Override
                 public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
-                   position = mListView.getItemIdAtPosition(i);
-                    int newPos = (int) position;
+                    lineNumber = mListView.getItemIdAtPosition(i);
+                    populateUI();
 
-
-                    testView.setText(new StringBuilder().append("clicked ").append(newPos).toString());
 
                 }
             });
@@ -120,12 +141,44 @@ public class MainActivity extends AppCompatActivity {
 
             setSimpleAdapter();
 
-            portTestView.setText("this is regestering in portrait");
+            mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int i, long id) {
+                    lineNumber = mSpinner.getItemIdAtPosition(i);
+                    populateUI();
+
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+
+                }
+            });
+
+
         }
 
 
     }
+    // shared ui population method
+    private void populateUI() {
+        int newPos = (int) lineNumber;
 
+        TextView name = (TextView) findViewById(R.id.WorkersName);
+        TextView jobRes = (TextView) findViewById(R.id.jobRespon);
+        TextView phone = (TextView) findViewById(R.id.WorkersPhone);
+        TextView bio = (TextView) findViewById(R.id.WorkersBio);
+
+        String mName = mWorkers.get(newPos).getName();
+        String mJobRes = mWorkers.get(newPos).getJobRes();
+        String mPhone = mWorkers.get(newPos).getPhone();
+        String mBio = mWorkers.get(newPos).getBio();
+
+        name.setText(mName);
+        bio.setText(mBio);
+        jobRes.setText(mJobRes);
+        phone.setText(mPhone);
+    }
 
 
 }
--- Workingwithadvancedviews/app/src/main/java/com/example/mich/workingwithadvancedviews/Workers.java
+++ Workingwithadvancedviews/app/src/main/java/com/example/mich/workingwithadvancedviews/Workers.java
@@ -5,23 +5,31 @@ public class Workers {
 
     // Member variables
     private String mName;
-    private String mDepartment;
+    private String mJobRes;
+    private String mPhone;
+    private String mBio;
 
 
     //creates the object
-    public Workers(String _name, String _department ) {
+    public Workers(String _name, String _jobRes, String _phone, String _bio ) {
 
         mName = _name;
-        mDepartment = _department;
+        mJobRes = _jobRes;
+        mPhone = _phone;
+        mBio = _bio;
 
     }
 
     // Getter methods.
     public String getName() { return mName;}
-    public String getDepartment() { return mDepartment;}
+    public String getJobRes() { return mJobRes;}
+    public String getPhone() { return mPhone;}
+    public String getBio() { return mBio;}
 
     // Setter Methods
     public void setName(String _name) { mName = _name; }
-    public void setDepartment(String _department) { mDepartment = _department; }
+    public void setJobRes(String _jobRes) { mJobRes = _jobRes; }
+    public void setPhone(String _Phone) { mPhone = _Phone; }
+    public void setBio(String _bio) {mBio = _bio;}
 
 }
