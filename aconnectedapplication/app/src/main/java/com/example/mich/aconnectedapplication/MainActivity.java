bb44602e2883868694631f42245380c7f5eb973e
mvoshell78 <michael.voshell@yahoo.com>
2015-12-17 16:07:34 -0500

    added an api

--- aconnectedapplication/app/src/main/java/com/example/mich/aconnectedapplication/MainActivity.java
+++ aconnectedapplication/app/src/main/java/com/example/mich/aconnectedapplication/MainActivity.java
@@ -6,10 +6,14 @@ import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import org.apache.commons.io.IOUtils;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -23,6 +27,17 @@ public class MainActivity extends AppCompatActivity {
 
     TextView myText;
     TextView myText2;
+    EditText address;
+    EditText city;
+    EditText state;
+    Button getGeo;
+
+    String cityInput;
+    String stateInput;
+    String addressInput;
+    String addressFinal;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,14 +46,42 @@ public class MainActivity extends AppCompatActivity {
         myText = (TextView) findViewById(R.id.textView);
 
         myText2 = (TextView) findViewById(R.id.textView2);
+        address = (EditText) findViewById(R.id.street);
+        city = (EditText) findViewById(R.id.city);
+        state = (EditText) findViewById(R.id.state);
+        getGeo = (Button) findViewById(R.id.getGeoButton);
+
+        getGeo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addressInput = String.valueOf(address.getText());
+                cityInput = String.valueOf(city.getText());
+                stateInput = String.valueOf(state.getText());
+                if (isOnline()) {
+                    backTask task = new backTask();
+                    task.execute();
+
+                    String[] a = addressInput.split(" +");
+
+                    makeAddressUsable(a);
+
+                } else {
+
+                }
+            }
+        });
 
 
 
 
-        if (isOnline()) {
-           backTask task = new backTask();
-            task.execute();
-        } else {
+    }
+
+    private void makeAddressUsable(String[] a) {
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < a.length; i++) {
+            sb.append(a[i] + "+");
+            addressFinal = sb.toString();
 
         }
     }
@@ -50,7 +93,7 @@ public class MainActivity extends AppCompatActivity {
         NetworkInfo netInfo = mgr.getActiveNetworkInfo();
 
         if (netInfo != null && netInfo.isConnectedOrConnecting()) {
-            Toast.makeText(this, "We're Connected", Toast.LENGTH_LONG).show();
+            //Toast.makeText(this, "We're Connected", Toast.LENGTH_LONG).show();
             return true;
         } else{
             Toast.makeText(this,"We're  not Connected", Toast.LENGTH_LONG).show();
@@ -70,14 +113,17 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected String doInBackground(String... params) {
             // The URL string that points to our web resource.
-            String urlString = "http://www.recipepuppy.com/api/";
+            String city = cityInput;
+            String state = stateInput;
+            String address = addressFinal;
+            String urlString = "http://www.yaddress.net/api/address?AddressLine1=" + address + "&AddressLine2=" + city + "+" + state;
 
             // Creating the URL object that points to our web resource.
             URL url = null;
 
             // Establish a connection to the resource at the URL.
             HttpURLConnection connection = null;
-            String resourceData = "test";
+            String resourceData = "No Data";
             try {
                 url = new URL(urlString);
                 connection = (HttpURLConnection) url.openConnection();
@@ -100,24 +146,42 @@ public class MainActivity extends AppCompatActivity {
                 // Cleaning up our connection resources.
                 stream.close();
                 connection.disconnect();
-                return resourceData;
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
 
+            JSONObject apiData;
+
+            try{
+                apiData = new JSONObject(resourceData);
+
+            } catch(Exception e){
+                System.out.println("Cannot convert API resonce to JSON");
+                apiData = null;
+            }
+
+            try{
+                apiData = (apiData!= null) ? apiData.getJSONObject("Number") : null;
+            } catch (Exception e){
+                System.out.println("could not parse data");
+                apiData = null;
+
+            }
 
 
-
-            return resourceData;
-
-            // The resourceData string should now have our data.
+            if (apiData != null) {
+                return apiData.toString();
+            }else{
+                return "there was a problem";
+            }
 
 
         }
 
         @Override
         protected void onPostExecute(String s) {
-            myText2.setText(s);
+          myText2.setText(s);
         }
     }
 
